ac2d5661ebcee33ea0e94f678189c0ec27eaf954
Michael Hansen <michael.hansen.24@us.af.mil>
2020-11-12 16:07:24 -0500

    Improve Librivox scripts

--- bin/librivox_align.py
+++ bin/librivox_align.py
@@ -1,10 +1,37 @@
 #!/usr/bin/env python3
+"""
+Uses Aeneas to align Librivox MP3 audio book files with book text.
+
+YAML file format:
+
+---
+aeneas:
+  language: <AENEAS LANGUAGE>
+
+gruut:
+  language: <GRRUT LANGUAGE>
+
+text:
+  file: <BOOK TEXT FILE NAME>
+
+audio:
+  <MP3 NAME>:
+    start_time: <SECONDS TO SKIP FROM START>
+    end_time: -<SECONDS TO SKIP FROM END>
+    start_line: <LINE TO START IN BOOK TEXT>
+    end_line: <LINE TO STOP IN BOOX TEXT>
+  <MP3 NAME>:
+    ...
+"""
 import argparse
 import logging
 from pathlib import Path
 
+import yaml
 from aeneas.executetask import ExecuteTask
 from aeneas.task import Task
+
+import gruut
 
 _LOGGER = logging.getLogger("librivox_align")
 
@@ -14,35 +41,77 @@ _LOGGER = logging.getLogger("librivox_align")
 def main():
     """Main entry point"""
     parser = argparse.ArgumentParser(prog="librivox_align.py")
-    parser.add_argument("input_dir", help="Directory with MP3 and text files")
-    parser.add_argument("--lang", required=True, help="Aeneas language")
+    parser.add_argument("book_yml", help="YAML file with book details")
     args = parser.parse_args()
-
-    args.input_dir = Path(args.input_dir)
 
     logging.basicConfig(level=logging.DEBUG)
     _LOGGER.debug(args)
 
-    config_string = (
-        f"task_language={args.lang}|is_text_type=plain|os_task_file_format=json"
-    )
+    args.book_yml = Path(args.book_yml)
+    input_dir = args.book_yml.parent
 
-    for mp3_path in args.input_dir.glob("*.mp3"):
-        text_path = mp3_path.with_suffix(".txt")
-        if not text_path.is_file():
-            _LOGGER.warning("Missing %s", text_path)
-            continue
+    with open(args.book_yml, "r") as book_file:
+        book = yaml.safe_load(book_file)
 
+    # Load gruut language
+    gruut_lang = gruut.Language.load(book["gruut"]["language"])
+    assert gruut_lang, "Unsupported language"
+
+    language = book["aeneas"]["language"]
+
+    # Load book text
+    text_path = Path(input_dir / book["text"]["file"])
+    _LOGGER.debug("Loading book text from %s", text_path)
+    with open(text_path, "r") as text_file:
+        text = text_file.readlines()
+
+    # Process MP3 files
+    for mp3_name, mp3_info in book["audio"].items():
+        mp3_path = input_dir / mp3_name
         sync_path = mp3_path.with_suffix(".json")
-        task = Task(config_string=config_string)
 
+        config_string = f"task_language={language}|is_text_type=plain|os_task_file_format=json|task_adjust_boundary_no_zero=True"
+
+        start_time = float(mp3_info.get("start_time", 0))
+        if start_time > 0:
+            # Skip seconds at the beginning
+            config_string += f"|is_audio_file_head_length={start_time}"
+
+        end_time = float(mp3_info.get("end_time", 0))
+        if end_time < 0:
+            # Skip seconds at the end
+            end_time = abs(end_time)
+            config_string += f"|is_audio_file_tail_length={end_time}"
+        elif end_time > 0:
+            # Set length of audio
+            config_string += f"|is_audio_file_process_length={end_time}"
+
+        task = Task(config_string=config_string)
         task.audio_file_path_absolute = mp3_path.absolute()
-        task.text_file_path_absolute = text_path.absolute()
         task.sync_map_file_path_absolute = sync_path.absolute()
 
-        _LOGGER.debug("Generating %s (%s %s)", sync_path, mp3_path, sync_path)
-        ExecuteTask(task).execute()
-        task.output_sync_map_file()
+        mp3_text_path = mp3_path.with_suffix(".txt")
+        with open(mp3_text_path, mode="w+") as mp3_text_file:
+            start_line, end_line = mp3_info["start_line"], mp3_info["end_line"]
+            # Clean up newlines in text
+            mp3_text = ""
+            for line_index in range(start_line - 1, end_line):
+                mp3_text += text[line_index].strip()
+
+            # Run through gruut tokenizer to expand abbreviations, numbers, etc.
+            for sentence in gruut_lang.tokenizer.tokenize(mp3_text):
+                clean_text = " ".join(sentence.clean_words)
+
+                # Each sentence in on a line now
+                print(clean_text, file=mp3_text_file)
+
+            mp3_text_file.seek(0)
+            task.text_file_path_absolute = mp3_text_file.name
+
+            # Generate sync map JSON file
+            _LOGGER.debug("Generating %s (%s)", sync_path, mp3_path)
+            ExecuteTask(task).execute()
+            task.output_sync_map_file()
 
 
 # -----------------------------------------------------------------------------
--- bin/librivox_split.py
+++ bin/librivox_split.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
+"""Splits Librivox MP3 audio book files into WAV/text fragments according to an Aeneas sync map."""
 import argparse
+import io
 import logging
 import json
 from pathlib import Path
 
+import yaml
 from pydub import AudioSegment
 
 _LOGGER = logging.getLogger("librivox_split")
@@ -14,10 +17,19 @@ _LOGGER = logging.getLogger("librivox_split")
 def main():
     """Main entry point"""
     parser = argparse.ArgumentParser(prog="librivox_split.py")
-    parser.add_argument("audio_file", help="MP3 audio file")
-    parser.add_argument("sync_file", help="Sync JSON map")
-    parser.add_argument("output_dir", help="Directory to write audio files")
-    parser.add_argument("--prefix", help="Prefix for audio files")
+    parser.add_argument("book_yml", help="YAML file with book details")
+    parser.add_argument(
+        "--before-ms",
+        type=int,
+        default=0,
+        help="Milliseconds before sync to keep (default: 0)",
+    )
+    parser.add_argument(
+        "--after-ms",
+        type=int,
+        default=0,
+        help="Milliseconds after sync to keep (default: 0)",
+    )
     parser.add_argument(
         "--min-ms",
         type=int,
@@ -26,42 +38,58 @@ def main():
     )
     args = parser.parse_args()
 
-    args.audio_file = Path(args.audio_file)
-    args.sync_file = Path(args.sync_file)
-    args.output_dir = Path(args.output_dir)
-
-    if not args.prefix:
-        args.prefix = args.audio_file.stem
-
     logging.basicConfig(level=logging.DEBUG)
     _LOGGER.debug(args)
 
-    args.output_dir.mkdir(parents=True, exist_ok=True)
+    args.book_yml = Path(args.book_yml)
+    input_dir = args.book_yml.parent
 
-    # Load MP3
-    audio = AudioSegment.from_mp3(args.audio_file)
-    with open(args.sync_file, "r") as sync_file:
-        sync_map = json.load(sync_file)
+    with open(args.book_yml, "r") as book_file:
+        book = yaml.safe_load(book_file)
 
-    # Write WAV files and transcriptions
-    i = 0
-    for fragment in sync_map["fragments"]:
-        begin_ms = float(fragment["begin"]) * 1000
-        end_ms = float(fragment["end"]) * 1000
+    for mp3_name, mp3_info in book["audio"].items():
+        # Load MP3
+        mp3_path = input_dir / mp3_name
+        audio = AudioSegment.from_mp3(mp3_path)
 
-        if (end_ms - begin_ms) >= args.min_ms:
-            fragment_path = args.output_dir / f"{args.prefix}_{i:03d}.wav"
-            fragment_audio = audio[begin_ms:end_ms]
-            fragment_audio.export(fragment_path, format="wav")
+        sync_path = mp3_path.with_suffix(".json")
+        with open(sync_path, "r") as sync_file:
+            sync_map = json.load(sync_file)
 
-            text_path = fragment_path.with_suffix(".txt")
-            with open(text_path, "w") as text_file:
-                for line in fragment["lines"]:
-                    line = line.strip()
-                    if line:
-                        print(line, file=text_file)
+        mp3_stem = mp3_path.stem
+        output_dir = input_dir / mp3_stem
+        output_dir.mkdir(parents=True, exist_ok=True)
 
-            i += 1
+        # Write WAV files and transcriptions
+        i = 0
+        for fragment in sync_map["fragments"]:
+            begin_ms = int(float(fragment["begin"]) * 1000)
+            end_ms = int(float(fragment["end"]) * 1000)
+
+            if (end_ms - begin_ms) >= args.min_ms:
+                with io.StringIO() as text_io:
+                    for line in fragment["lines"]:
+                        line = line.strip()
+                        if line:
+                            print(line, file=text_io)
+
+                    text = text_io.getvalue().strip()
+
+                if text:
+                    fragment_path = output_dir / f"{mp3_stem}_{i:03d}.wav"
+
+                    # Write text (transcription)
+                    text_path = fragment_path.with_suffix(".txt")
+                    text_path.write_text(text)
+
+                    # Export audio
+                    begin_ms -= args.before_ms
+                    end_ms += args.after_ms
+
+                    fragment_audio = audio[begin_ms:end_ms]
+                    fragment_audio.export(fragment_path, format="wav")
+
+                    i += 1
 
 
 # -----------------------------------------------------------------------------
