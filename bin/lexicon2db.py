9e970aba03d05593676d7be6d40cc6abc413eeeb
Michael Hansen <michael.hansen.24@us.af.mil>
2021-03-23 16:49:44 -0400

    Require casing in lexicon2db

--- bin/lexicon2db.py
+++ bin/lexicon2db.py
@@ -9,13 +9,32 @@ def main():
     """Main entry point"""
     parser = argparse.ArgumentParser(prog="lexicon2db.py")
     parser.add_argument(
-        "--pos", action="store_true", help="Lexicon includes part of speech"
+        "--casing",
+        required=True,
+        choices=("keep", "lower", "upper"),
+        help="Casing to apply to words",
     )
     parser.add_argument(
-        "lexicon", help="Text lexicon to read with <WORD> <PHONEME> <PHONEME> ..."
+        "--lexicon",
+        required=True,
+        help="Text lexicon to read with <WORD> <PHONEME> <PHONEME> ...",
     )
-    parser.add_argument("database", help="SQLite database to write")
+    parser.add_argument("--database", required=True, help="SQLite database to write")
+    parser.add_argument(
+        "--pos", action="store_true", help="Lexicon includes part of speech"
+    )
     args = parser.parse_args()
+
+    # -------------------------------------------------------------------------
+
+    word_casing = lambda x: x
+
+    if args.casing == "lower":
+        word_casing = str.lower
+    elif args.casing == "upper":
+        word_casing = str.upper
+
+    # -------------------------------------------------------------------------
 
     conn = sqlite3.connect(args.database)
     conn.execute("DROP TABLE IF EXISTS word_phonemes")
@@ -47,6 +66,7 @@ def main():
                 word, phonemes = line.split(maxsplit=1)
                 pos = ""
 
+            word = word_casing(word)
             pron_order = pron_orders.get(word, 0)
 
             conn.execute(
