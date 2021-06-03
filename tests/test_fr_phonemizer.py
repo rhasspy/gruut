86a006be5427a929172614eed67c5c530e57ebd3
Michael Hansen <michael.hansen.24@us.af.mil>
2021-06-03 13:41:03 -0400

    Add more liason tests

--- tests/test_fr_phonemizer.py
+++ tests/test_fr_phonemizer.py
@@ -1,10 +1,44 @@
 #!/usr/bin/env python3
 """Tests for FrenchPhonemizer class"""
+import typing
 import unittest
 
 from gruut import text_to_phonemes, Token
 from gruut.lang import get_phonemizer, get_tokenizer
 
+# # https://www.commeunefrancaise.com/blog/la-liaison
+
+# # After a determiner
+# Un enfant.
+# Les arbres.
+# Deux amis.
+# Ton excellent vin.
+# Ces autres voyages.
+
+# # Pronoun + verb
+# On est là!
+# Elles ont faim!
+# Vous êtes sûrs?
+# Tu nous entends.
+# Je les adore.
+
+# # Adjective + noun
+# J’ai des petites oreilles.
+# Michel est un grand ami.
+# Je regarde la télé sur un petit écran.
+# C’est un ancien élève.
+
+# # After short prepositions, and “très”
+# C’est très amusant!
+# Je vis en Amérique.
+# Ils sont chez eux.
+# J’arrive dans une minute.
+
+# # Others
+# Un bâtiment est en vue de l’île.
+# Sa vie n’était pas en danger.
+# Amalia est en danger.
+# C`est incroyable!
 
 class FrenchPhonemizerTestCase(unittest.TestCase):
     """Test cases for FrenchPhonemizer class"""
@@ -12,31 +46,63 @@ class FrenchPhonemizerTestCase(unittest.TestCase):
     def test_liason(self):
         """Test addition of liason"""
         tokenizer = get_tokenizer("fr-fr")
-        sentence = next(tokenizer.tokenize("J’ai des petites oreilles."))
 
-        liason_word = "petites"
-        self.assertIn(liason_word, sentence.clean_words)
+        # After a determiner
+        sentence = self._without_and_with_liason(
+            "Les arbres", "les", ["l", "e"], ["l", "e", "z"]
+        )
 
-        # Verify no liason
-        phonemizer_no_liason = get_phonemizer("fr-fr", fr_no_liason=True)
-        phonemes_no_liason = next(phonemizer_no_liason.phonemize(sentence.tokens))
+        # Pronoun + verb
+        sentence = self._without_and_with_liason(
+            "On est là!", "on", ["ɔ̃"], ["ɔ̃", "n"]
+        )
 
-        for word, word_phonemes in zip(sentence.clean_words, phonemes_no_liason):
-            if word == liason_word:
-                self.assertEqual(phonemes, ["p", "ə", "t", "i", "t"])
+        # Adjective + noun
+        sentence = self._without_and_with_liason(
+            "J’ai des petites oreilles.",
+            "petites",
+            ["p", "ə", "t", "i", "t"],
+            ["p", "ə", "t", "i", "t", "z"],
+        )
 
-        # Verify liason
-        phonemizer_liason = get_phonemizer("fr-fr")
-        phonemes_liason = next(phonemizer_liason.phonemize(sentence.tokens))
-
-        for word, word_phonemes in zip(sentence.clean_words, phonemes_no_liason):
-            if word == liason_word:
-                self.assertEqual(phonemes, ["p", "ə", "t", "i", "t", "z"])
+        # After short prepositions, and “très”
+        sentence = self._without_and_with_liason(
+            "C’est très amusant!", "très", ["t", "ʁ", "ɛ"], ["t", "ʁ", "ɛ", "z"]
+        )
 
     def test_last_token(self):
         """Ensure liason does not leave last token"""
         phonemes = text_to_phonemes("Est-ce-que", lang="fr")
         self.assertGreater(len(phonemes), 0)
+
+    def _without_and_with_liason(
+        self,
+        text: str,
+        liason_word: str,
+        without_phonemes: typing.List[str],
+        with_phonemes: typing.List[str],
+    ):
+        """Get pronunciation of a sentence with and without liason enabled"""
+        tokenizer = get_tokenizer("fr-fr")
+        sentence = next(tokenizer.tokenize(text))
+
+        self.assertIn(liason_word, sentence.clean_words)
+
+        # Verify no liason
+        phonemizer_no_liason = get_phonemizer("fr-fr", fr_no_liason=True)
+        phonemes_no_liason = list(phonemizer_no_liason.phonemize(sentence.tokens))
+        for word, word_phonemes in zip(sentence.clean_words, phonemes_no_liason):
+            if word == liason_word:
+                self.assertEqual(word_phonemes, without_phonemes)
+
+        # Verify liason
+        phonemizer_liason = get_phonemizer("fr-fr")
+        phonemes_liason = list(phonemizer_liason.phonemize(sentence.tokens))
+
+        for word, word_phonemes in zip(sentence.clean_words, phonemes_liason):
+            if word == liason_word:
+                self.assertEqual(word_phonemes, with_phonemes)
+
 
 # -----------------------------------------------------------------------------
 
--- tests/test_lang.py
+++ tests/test_lang.py
@@ -8,8 +8,8 @@ from gruut.lang import id_to_phonemes
 class GruutLangTestCase(unittest.TestCase):
     """Test cases for gruut.lang"""
 
-    def test_id_to_phonemes(self):
-        """Test id_to_phonemes"""
+    def test_id_to_phonemes_en(self):
+        """Test id_to_phonemes to U.S. English"""
         phonemes = id_to_phonemes("en-us")
         self.assertEqual(
             phonemes,
@@ -60,7 +60,74 @@ class GruutLangTestCase(unittest.TestCase):
                 "ʌ",
                 "ʒ",
                 "θ",
-            ]
+            ],
+        )
+
+    def test_id_to_phonemes_nl(self):
+        """Test id_to_phonemes for Dutch"""
+        phonemes = id_to_phonemes("nl")
+        self.assertEqual(
+            phonemes,
+            [
+                "_",  # pad
+                "|",  # minor break
+                "‖",  # major break
+                "#",  # word break
+                "ˈ",  # primary stress
+                "ˌ",  # secondary stress
+                "a",
+                "aː",
+                "b",
+                "c",
+                "d",
+                "e",
+                "eː",
+                "f",
+                "h",
+                "i",
+                "iː",
+                "j",
+                "k",
+                "l",
+                "m",
+                "n",
+                "o",
+                "oː",
+                "p",
+                "s",
+                "t",
+                "u",
+                "uː",
+                "v",
+                "w",
+                "x",
+                "y",
+                "yː",
+                "z",
+                "ø",
+                "ŋ",
+                "œy",
+                "œː",
+                "ɑ",
+                "ɑu",
+                "ɑː",
+                "ɔ",
+                "ɔː",
+                "ə",
+                "ɛ",
+                "ɛi",
+                "ɛː",
+                "ɡ",
+                "ɣ",
+                "ɪ",
+                "ɱ",
+                "ɹ",
+                "ʃ",
+                "ʏ",
+                "ʏː",
+                "ʒ",
+                "ʔ",
+            ],
         )
 
 
