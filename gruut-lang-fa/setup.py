92def45488940803cf7e453def03c6d34e7f4fef
Michael Hansen <mike@rhasspy.org>
2021-07-06 17:02:04 -0400

    Add fa package

---
+++ gruut-lang-fa/setup.py
@@ -0,0 +1,41 @@
+"""Setup file for gruut_lang_fa"""
+import os
+from pathlib import Path
+
+import setuptools
+
+module_name = "gruut_lang_fa"
+
+this_dir = Path(__file__).parent
+module_dir = this_dir / module_name
+
+# -----------------------------------------------------------------------------
+
+version_path = module_dir / "VERSION"
+with open(version_path, "r") as version_file:
+    version = version_file.read().strip()
+
+# -----------------------------------------------------------------------------
+
+setuptools.setup(
+    name=module_name,
+    description="Farsi language files for gruut tokenizer/phonemizer",
+    version=version,
+    author="Michael Hansen",
+    author_email="mike@rhasspy.org",
+    url="https://github.com/rhasspy/gruut",
+    packages=setuptools.find_packages(),
+    package_data={
+        module_name: [
+            "VERSION",
+            "lexicon.db",
+            "g2p/model.crf",
+            "espeak/lexicon.db",
+            "espeak/g2p/model.crf",
+        ]
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
