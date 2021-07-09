5903751c1ac4321c173893fa33ef9767c180b107
Michael Hansen <mike@rhasspy.org>
2021-07-09 16:11:11 -0400

    Add speaker to clean_metadata script

--- bin/clean-metadata.py
+++ bin/clean-metadata.py
@@ -1,21 +1,33 @@
 #!/usr/bin/env python3
+import argparse
 import csv
 import sys
 
 from gruut.lang import get_tokenizer
 
-if len(sys.argv) < 2:
-    print("Usage: clean-metadata.py <lang> < CSV > CSV")
+parser = argparse.ArgumentParser(prog="clean_metadata.py")
+parser.add_argument("lang", help="Language code")
+parser.add_argument(
+    "--has-speaker", action="store_true", help="CSV input has format id|speaker|text"
+)
+args = parser.parse_args()
 
-lang = sys.argv[1]
-tokenizer = get_tokenizer(lang)
+tokenizer = get_tokenizer(args.lang)
 
 writer = csv.writer(sys.stdout, delimiter="|")
 for row in csv.reader(sys.stdin, delimiter="|"):
-    utt_id, text = row[0], row[1]
+    speaker = None
+
+    if args.has_speaker:
+        utt_id, speaker, text = row[0], row[1], row[2]
+    else:
+        utt_id, text = row[0], row[1]
 
     words = []
     for sentence in tokenizer.tokenize(text):
         words.extend(word for word in sentence.clean_words if tokenizer.is_word(word))
 
-    writer.writerow((utt_id, " ".join(words)))
+    if args.has_speaker:
+        writer.writerow((utt_id, speaker, " ".join(words)))
+    else:
+        writer.writerow((utt_id, " ".join(words)))
