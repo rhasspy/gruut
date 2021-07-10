6e261552db2f18f7cdac9391618d3b0b29e19b99
Michael Hansen <hansen.mike@gmail.com>
2021-07-09 20:11:21 -0400

    Fix PYTHONPATH in tests

--- scripts/test-lang-dirs.sh
+++ scripts/test-lang-dirs.sh
@@ -12,6 +12,11 @@ fi
 
 export PYTHONPATH="${src_dir}"
 
+while read -r lang_dir;
+do
+    export PYTHONPATH="${lang_dir}:${PYTHONPATH}"
+done < <(find "${src_dir}" -maxdepth 1 -type d -name 'gruut-lang-*')
+
 # -----------------------------------------------------------------------------
 
 function gruut {
