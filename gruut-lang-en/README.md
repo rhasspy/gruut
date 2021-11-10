c742711d25fad0ec963067527dac52ccb70f80c4
Michael Hansen <mike@rhasspy.org>
2021-11-10 11:13:12 -0500

    Split out en-us data files

---
+++ gruut-lang-en/README.md
@@ -0,0 +1,3 @@
+# gruut English
+
+Language-specific files for English (en) in [gruut](https://github.com/rhasspy/gruut)
---
+++ gruut-lang-en/gruut_lang_en/__init__.py
@@ -0,0 +1,9 @@
+"""English language resources"""
+from pathlib import Path
+
+_DIR = Path(__file__).parent
+
+
+def get_lang_dir() -> Path:
+    """Get directory with language resources"""
+    return _DIR
