732f4b7d4fe0855bafb260b5fb85b4065cfaff89
Federico Costa <federico.costa@veue02.tsc.upc.edu>
2023-07-07 11:26:50 +0200

    deleted data/ca folder to enable pr

--- data/ca/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Catalan language resources"""
-from pathlib import Path
-
-_DIR = Path(__file__).parent
-
-def get_lang_dir() -> Path:
-    """Get directory with language resources"""
-    return _DIR
