09be7b0547da9681578f8d9b66e546254d59e672
Federico Costa <federico.costa@veue02.tsc.upc.edu>
2023-10-20 13:28:47 +0200

    added ce and ba files

---
+++ gruut-lang-ca/gruut_lang_ca/__init__.py
@@ -0,0 +1,22 @@
+"""Catalan language resources"""
+import os
+import typing
+from pathlib import Path
+
+try:
+    import importlib.resources
+
+    files = importlib.resources.files
+except (ImportError, AttributeError):
+    # Backport for Python < 3.9
+    import importlib_resources  # type: ignore
+
+    files = importlib_resources.files
+
+_PACKAGE = "gruut_lang_ca"
+_DIR = Path(typing.cast(os.PathLike, files(_PACKAGE)))
+
+
+def get_lang_dir() -> Path:
+    """Get directory with language resources"""
+    return _DIR
